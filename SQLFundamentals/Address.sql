3d5113e7cea041c2ce1881a07f4e5a2e8521e016
Arturo <Arturo_Osorio1@epam.com>
2024-01-11 14:37:17 -0600

    Add missing validations

--- SQLFundamentals/Address.sql
+++ SQLFundamentals/Address.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Street] NVARCHAR(50) NOT NULL, 
-    [City] NVARCHAR(50) NULL, 
+    [City] NVARCHAR(20) NULL, 
     [State] NVARCHAR(50) NULL, 
     [ZipCode] NVARCHAR(50) NULL
 )
---
+++ SQLFundamentals/EmployeeViewInfo.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[EmployeeViewInfo] AS 
+SELECT  
+	E.Id AS EmployeeId,
+	CASE
+		WHEN P.FirstName IS NOT NULL THEN P.FirstName
+		ELSE P.LastName 
+	END AS EmployeeFullNameEmployeeFullName,
+	A.ZipCode +'_'+ A.State+'_'+ A.City+'-'+A.Street AS EmployeeFullAddress,
+	LEFT(E.CompanyName, 20)+E.Position AS EmployeeCompanyInfo
+FROM Employee E
+INNER JOIN Address A ON A.Id = E.AddressId
+INNER JOIN Person P ON P.Id = E.PersonId
+ORDER BY E.CompanyName, A.City ASC offset 0 rows
